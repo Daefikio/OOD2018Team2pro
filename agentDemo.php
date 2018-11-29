b43f85346387c7ae2e87c63758e7b720771fb54e
yuuuchu <44778601+yuuuchu@users.noreply.github.com>
2018-11-28 17:53:18 -0700

    Update agentDemo.php

--- agentDemo.php
+++ agentDemo.php
@@ -26,6 +26,7 @@ data for registration.-->
 		<div class="-wrap -main-wrap">
 			<aside class="contact-form">
           <div id="container1">
+            <h1>Agent Bio</h1>
             <img class="profilePic" src="img/grumpyCat.jpg" alt="AgentPhoto" >
             <p class="agtInfo">Hello, my name is Chad and I am a Junior Agent at Travel Experts. My favorite thing to do is to make travel packages and customize CSS stylesheets. When I am not making travel website and packages, I am decorating cakes at my grandmother's house. Now, before you ask, I am not angry. I am just tired from coding and debuggin all of my programs. Messy code does not impress me, and my mission objective is to spend as much time as I can playing around in bouncy castle. <br /> My favorite place to travel is to France to see the feline and friends circus. I highly recommend it. My favorite food is responsibility sourced salmon from fancy feast. The quickest way to my heart is to organize code in a KISS (Keep It Simple Sweetie) way.
             </p>
@@ -33,18 +34,18 @@ data for registration.-->
         <!--Chad's Form-->
           <div id="container2">
               <h2>Connect with Agent</h2>
-              <form id="form2" method="post" action="contactfunction.php">
+              <form id="form2" method="post" action="bouncer.php">
                     <label class="tags">First Name:</label>
-                    <input type="text" name="custInqFName" required="required" id="Fname" />
+                    <input type="text" name="custInqFName" required="required" id="custInqFName" />
                     <br />
                     <label class="tags">Last Name:</label>
-                    <input type="text" name="custInqLName" required="required" id="Lname" />
+                    <input type="text" name="custInqLName" required="required" id="custInqLName" />
                     <br />
                     <label class="tags">Email Address:</label>
-                    <input type="text" name="custInqEmail" required="required" id="emailAddress" />
+                    <input type="text" name="custInqEmail" required="required" id="custInqEmail" />
                     <br />
                     <label class="tags">Phone Number:</label>
-                    <input type="text" name="custIngPhone" required="required" id="phoneNumber" />
+                    <input type="text" name="custIngPhone" required="required" id="custInqPhone" />
                     <br />
                     <label class="tags">Message:</label><br />
                     <textarea name="message" required="required" id="message" rows="5"></textarea>
